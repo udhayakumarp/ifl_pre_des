f59b1cd6dfba89cf428e40a695ef604cd76102e7
Udhaya <udhaya@teampumpkin.com>
2018-08-10 23:03:40 +0530

    sdfsdfsdf

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-//Path Location Strategy
-
-app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
